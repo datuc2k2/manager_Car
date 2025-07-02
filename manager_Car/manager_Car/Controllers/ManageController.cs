9107224cea68f7520498b7cf5d181024c0f1530c
TienDat02 <datnguyen83.fpt@gmail.com>
2025-07-02 22:48:14 +0700

    update FE

---
+++ manager_Car/manager_Car/Controllers/ManageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace manager_Car.Controllers
+{
+    public class ManageController : Controller
+    {
+        public IActionResult AccountList()
+        {
+            return View();
+        }
+    }
+}
