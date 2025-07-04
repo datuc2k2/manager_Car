2b4867b85485ba6fd0c5fe6dfa637b2cbbb97c12
TienDat02 <datnguyen83.fpt@gmail.com>
2025-07-04 16:21:12 +0700

    crud , login , add date

--- manager_Car/manager_Car/Controllers/LoginController.cs
+++ manager_Car/manager_Car/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using manager_Car.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace manager_Car.Controllers
 {
@@ -7,6 +8,36 @@ namespace manager_Car.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Login(AdminLoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Kiểm tra thông tin đăng nhập
+                if (model.user_name == "administrator" && model.user_password == "administrator")
+                {
+                    var token = "sample-token-" + System.Guid.NewGuid().ToString();
+
+                    // Lưu token vào cookie
+                    Response.Cookies.Append("AuthToken", token, new CookieOptions
+                    {
+                        HttpOnly = true,
+                        Secure = true,
+                        SameSite = SameSiteMode.Strict,
+                        Expires = DateTimeOffset.Now.AddHours(1)
+                    });
+
+                    return Json(new { success = true, token = token, redirectUrl = Url.Action("Index", "Home") });
+                }
+
+                else
+                {
+                    // Trả về lỗi
+                    return Json(new { success = false, message = "Tên đăng nhập hoặc mật khẩu không đúng!" });
+                }
+            }
+            return Json(new { success = false, message = "Dữ liệu không hợp lệ!" });
         }
     }
 }
--- manager_Car/manager_Car/Models/AdminLoginModel.cs
+++ manager_Car/manager_Car/Models/AdminLoginModel.cs
@@ -1,8 +1,14 @@
-﻿namespace manager_Car.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace manager_Car.Models
 {
     public class AdminLoginModel
     {
+        [Required(ErrorMessage = "Vui lòng nhập tên đăng nhập")]
         public string user_name { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        [DataType(DataType.Password)]
         public string user_password { get; set; }
     }
 }
